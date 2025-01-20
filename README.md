891772b256ea1423d9e8018de46d3e4691c5e7e7
Aleksei <alekseirodinkov21@gmail.com>
2025-01-20 18:02:22 +0300

    buildScript updated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # VkBack
+### main.cc - (Непараллельный алгоритм Дейкстры) Метод запуска - UNIX: ./build.sh main.cc -> app ; - Windows : - TODO
+### mainP.cc - (Распараллеленный алгоритм Дейкстры) Метотд запуска - UNIX:  ./build.sh mainP.cc -> app; 
--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
-g++ -Wall -Werror -std=c++17 -Wpedantic -O2 -o app main.cc
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Ошибка: укажите имя файла для компиляции."
+    echo "Пример использования: ./build.sh main.cc"
+    exit 1
+fi
+
+SOURCE_FILE=$1
+
+g++ -Wall -Werror -std=c++17 -Wpedantic -O2 -o app "$SOURCE_FILE"
+
